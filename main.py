c29d9a3fed53e09372fe4fbece9e6489730e91a7
miyabi <miyabi.oguchi@gmail.com>
2025-03-29 20:38:42 +0900

    1st commit

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-from PIL import Image
 import time
 
 st.title('Streamlit 超入門')
@@ -30,15 +27,4 @@ expander2 = st.expander('問い合わせ2')
 expander2.write('問い合わせ2の回答')
 expander3 = st.expander('問い合わせ3')
 expander3.write('問い合わせ3の回答')
-
-
-# text = st.text_input('あなたの趣味を教えて下さい。')
-# condition = st.slider('あなたの今の調子は？', 0, 100, 50)
-
-# 'あなたの趣味：', text
-# 'コンディション：', condition
-
-# if st.checkbox('Show Image'):
-#     img = Image.open('2023 May birthday Ornament.jpg')
-#     st.image(img, caption='Happy Birthday', use_container_width=True)
 
